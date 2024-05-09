f853ed0a6f7bdb8aea0fcb93e4833a7ffac0b3d3
dwarowski <realcarenthusiast@yandex.ru>
2024-05-09 22:24:01 +0300

    new hw

--- Homework_04_01/Pascals_Triangle.cs
+++ Homework_04_01/Pascals_Triangle.cs
@@ -10,6 +10,40 @@ namespace Homework_04_01
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество строк треугольника паскаля: ");
+            int N = int.Parse(Console.ReadLine());
+            int windowWidth = Console.WindowWidth / 2;
+
+            int[][] nums = new int[N + 1][];
+
+            for (int i = 1; i < N + 1; i++)
+            {
+                
+                Console.SetCursorPosition(windowWidth, i + 1);  // Установка курсора консоли по центру для вывода треугольника
+                windowWidth -= 2;                               // Сдвиг следющих цифр, чтобы получилась пирамида
+
+                nums[i] = new int[i + 1];                       // Создание нового ряда в зубчатом массиве 
+                nums[i][0] = 1;                                 // Начальная еденица пирамиды
+                Console.Write($" {nums[i][0], 2} ");            // Вывод начальной единцы 
+
+
+                for (int j = 0;  j < i + 1; j++)
+                {
+                   
+                    
+                    Console.Write($" {nums[i][j], 2} ");
+                    
+                }
+
+                nums[i][i + 1] = 1;                             // Конечная еденица пирамиды
+                Console.Write($" {nums[i][i + 1],2} ");         // Вывод последней единцы 
+                Console.WriteLine();
+
+            }
+
+
+            Console.ReadKey();
+
         }
     }
 }
