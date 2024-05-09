dbae9832ed1d8e34801354cb7ee5ae2a98358096
dwarowski <realcarenthusiast@yandex.ru>
2024-05-09 20:44:19 +0300

    update structure

---
+++ Homework_04_01/Pascals_Triangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_04_01
+{
+    internal class Pascals_Triangle
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
