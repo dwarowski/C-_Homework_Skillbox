7c046586fbf133dc2ba9de36eaffe7ba40374513
dwarowski <realcarenthusiast@yandex.ru>
2024-05-16 20:34:21 +0300

    new hw

---
+++ Homework_04_02/Matrix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_04_02
+{
+    internal class Matrix
+    {
+        public int matrixRow { get; set; } = 0;
+        public int matrixColumn { get; set; } = 0;
+
+
+        int[,] matrix = new int[matrixRow];
+        public void MltiplyByNum(int num = 0)
+        {
+
+        }
+    }
+}
