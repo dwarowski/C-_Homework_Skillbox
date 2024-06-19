facfc95789a32c16150d2b1e8194dd5d914fbe53
dwarowski <realcarenthusiast@yandex.ru>
2024-06-19 16:44:32 +0300

    done multiply matrix by num

--- Homework_04_02/Matrix.cs
+++ Homework_04_02/Matrix.cs
@@ -10,36 +10,65 @@ namespace Homework_04_02
     internal class Matrix
     {
         Random r = new Random();
-        public void MltiplyByNum(int matrixRow = 0, int matrixColumn = 0)
+        public void MltiplyByNum()   // Умножение числа на матрицу 
         {
+            int matrixRow = 0;                                          // Число рядов матрицы
+            int matrixColumn = 0;                                       // Число колонок матрицы
+
             Console.Write("Введите количество рядов матрицы: ");
-            matrixRow = int.Parse(Console.ReadLine());            // Считываем кол-во рядов
+            matrixRow = int.Parse(Console.ReadLine());                  // Считываем кол-во рядов
             Console.Write("Введите количество колонок матрицы: ");
-            matrixColumn = int.Parse((Console.ReadLine()));       // Считываем кол-во колонок 
+            matrixColumn = int.Parse((Console.ReadLine()));             // Считываем кол-во колонок 
             Console.Write("Введите число на которое надо умножить: ");
-            int matrixNumMultiplier = int.Parse(Console.ReadLine());    // Число на которое будет умножатся матрица
+
             
-            int[,] matrix = new int[matrixRow, matrixColumn];     // Создаем матрицу по заданым значениям
+            int matrixNumMultiplier = int.Parse(Console.ReadLine());    // Число на которое будет умножатся матрица
+            int[,] matrix = new int[matrixRow, matrixColumn];           // Создаем матрицу по заданым значениям
             
             Console.Clear();
-            Console.SetCursorPosition(matrixRow / 2, 0);
-            Console.Write($"{matrixNumMultiplier} * ");
+            Console.SetCursorPosition(0, matrixRow / 2);                // Центровка числа в консоли
+            Console.Write($"{matrixNumMultiplier} * ");                 // Вывод числа 
+            
 
-
-            for (int i = 0; i < matrixRow; i++)              // Перебор циклом кадого элемента матрицы
+            for (int i = 0; i < matrixRow; i++)                         // Перебор циклом кадого элемента матрицы
             {
+                Console.SetCursorPosition(4, i);                        // Отступ матрицы в консоли
+                Console.Write("| ");
 
                 for (int j = 0 ; j < matrixColumn; j++)
                 {
-                    matrix[i, j] = r.Next(0,100);           // Заполняем рандомно матрицу
-                    Console.Write($"{matrix[i, j]} "); 
+                    matrix[i, j] = r.Next(0,100);                       // Заполняем рандомно матрицу
+                    Console.Write($"{matrix[i, j]} ");                  // Выводим матрицу в консоль
                 }
-                Console.WriteLine();
+
+                Console.WriteLine($"|");
             }
-            
-            ///matrix[i, j] *= matrixNumMultiplier;
+
+            Console.SetCursorPosition(7 + (3 * matrixColumn), matrixRow / 2);       // Отступ равно в консоли
+            Console.Write("=");                                                     // То самое равно
+
+
+            for (int i = 0; i < matrixRow; i++)                                     // Перебор циклом кадого элемента матрицы
+            {
+                Console.SetCursorPosition(5 + (4 * matrixColumn), i);               // Отступ перемноженной матрицы в консоли
+                Console.Write("| ");
+
+                for (int j = 0; j < matrixColumn; j++)
+                {
+                    matrix[i, j] *= matrixNumMultiplier;                // Перемножение матрицы
+                    Console.Write($"{matrix[i, j]} ");                  // Вывод перемноженной матрицы
+                }
+                Console.WriteLine("|");
+            }
 
             Console.ReadKey();
+            Console.Clear();
+        }
+        
+        
+        
+        public void MatrixAddMatrix()
+        {
 
         }
     }
--- Homework_04_02/Program.cs
+++ Homework_04_02/Program.cs
@@ -10,32 +10,46 @@ namespace Homework_04_02
     {
         static void Main(string[] args)
         {
+            int functionNum = 1;
+
             Console.WriteLine("Возможные действия:");
             Console.WriteLine("1. Умножить матрицу на число");
             Console.WriteLine("2.");
             Console.WriteLine("3.");
             Console.WriteLine("4.");
             Console.Write("Для выбора, напишиите цифру нужного действия: ");
-            int functionNum = int.Parse(Console.ReadLine());    // Читаем действие
-            Console.Clear();
 
+            functionNum = int.Parse(Console.ReadLine());                // Читаем действие
+            if (functionNum >= 1 && functionNum <= 4) 
+            {
+                Console.Clear();
+            }
+            else                    // Перезапускаем если неправильно введена цифра 
+            {
+                Console.Write("Несуществующее действие");
+                Console.ReadKey();
+                Console.Clear();
+                Main(args);
+            }
+               
             Matrix matrix = new Matrix();       // Создание новой матрицы
 
             if (functionNum == 1)
             {
-                matrix.MltiplyByNum();          // Умножение числа на матрицу       
+                matrix.MltiplyByNum();          // Умножение числа на матрицу
+                Main(args);                     // Перезапуск
             }
             if (functionNum == 2)
             {
-
+                Main(args);
             }
             if (functionNum == 3)
             {
-
+                Main(args);
             }
             if (functionNum == 4)
             {
-
+                Main(args);
             }
         }
     }
