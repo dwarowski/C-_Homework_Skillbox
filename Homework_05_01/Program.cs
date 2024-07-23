9f5f3a112ecdeda22462053187271e57286c1aee
dwarowski <realcarenthusiast@yandex.ru>
2024-07-23 16:49:00 +0300

    upd hw 05_01

--- Homework_05_01/Program.cs
+++ Homework_05_01/Program.cs
@@ -11,18 +11,60 @@ namespace Homework_05_01
         static void Main(string[] args)
         {
             string inputString = Console.ReadLine();
-
-            Console.WriteLine(MinLenWord(inputString));
-
+            foreach( string word in MinLenWord(inputString))
+            {
+                if (word != null)
+                {
+                    Console.WriteLine(word);
+                }
+                
+            }
+            foreach (string word in MaxLenWord(inputString))
+            {
+                if (word != null)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+            
             Console.ReadKey();
 
-           string MinLenWord(string input)
-           {
-                string[] words = input.Split(' ');
-                return words.;
-           }
 
+            
+            string[] MaxLenWord(string input)   // Поиск слова максимальной длины
+            {
+                string[] words = input.Split(' ');          // Делим входную строку в массив по пробелам
+                int maxLengthWord = words.Max(w => w.Length);   // Ищем максимальную длину слова
+                string[] maxWords = new string[words.Length];
+                
+                foreach (string word in words)          // Ищем все подходящие в массиве и добовляем в выводной массив
+                {
+                    if (word.Length == maxLengthWord)
+                    {
+                        maxWords.SetValue(word,1);
+                    }
+                }
+                return maxWords;
+            }
+
+            string[] MinLenWord(string input)   // Поиск слова минимальной длины
+            {
+                string[] words = input.Split(' ');          // Делим входную строку в массив по пробелам
+                int minLengthWord = words.Min(w => w.Length );   // Ищем минимальную длину слова
+                string[] minWords = new string[words.Length];
+
+                foreach (string word in words)          // Ищем все подходящие в массиве и добовляем в выводной массив
+                {
+                    if (word.Length == minLengthWord)
+                    {
+                        minWords.Append(word);
+                    }
+                }
+                return minWords;
+            }
         }
-       
+
     }
+       
 }
+
