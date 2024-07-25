cc6183f08dca6c76f1f3eff4f4d95c601ad50c7a
dwarowski <realcarenthusiast@yandex.ru>
2024-07-25 22:41:07 +0300

    almost finished hw part3

--- Homework_05_01/Program.cs
+++ Homework_05_01/Program.cs
@@ -11,53 +11,40 @@ namespace Homework_05_01
         static void Main(string[] args)
         {
             string inputString = Console.ReadLine();
-            foreach( string word in MinLenWord(inputString))
-            {
-                if (word != null)
-                {
-                    Console.WriteLine(word);
-                }
-                
-            }
-            foreach (string word in MaxLenWord(inputString))
-            {
-                if (word != null)
-                {
-                    Console.WriteLine(word);
-                }
-            }
+
+            Console.WriteLine(MinLenWord(inputString));
+
+            Console.WriteLine(MaxLenWord(inputString));             
             
             Console.ReadKey();
 
-
             
-            string[] MaxLenWord(string input)   // Поиск слова максимальной длины
+            string MaxLenWord(string input)   // Поиск слова максимальной длины
             {
-                string[] words = input.Split(' ');          // Делим входную строку в массив по пробелам
+                string[] words = input.Split(' ');              // Делим входную строку в массив по пробелам
                 int maxLengthWord = words.Max(w => w.Length);   // Ищем максимальную длину слова
-                string[] maxWords = new string[words.Length];
-                
-                foreach (string word in words)          // Ищем все подходящие в массиве и добавляем в выводной массив
+                string maxWords = "";                           // Пустая выводная строка
+                foreach (string word in words)                  // Ищем все подходящие в массиве и добавляем в выводной массив
                 {
                     if (word.Length == maxLengthWord)
                     {
-                        maxWords.SetValue(word,1);
+                       maxWords += $"{word} ";                  // Добавляем к выводной строке максимальное слово
                     }
                 }
                 return maxWords;
             }
 
-            string[] MinLenWord(string input)   // Поиск слова минимальной длины
+            string MinLenWord(string input)   // Поиск слова минимальной длины
             {
-                string[] words = input.Split(' ');          // Делим входную строку в массив по пробелам
+                string[] words = input.Split(' ');               // Делим входную строку в массив по пробелам
                 int minLengthWord = words.Min(w => w.Length );   // Ищем минимальную длину слова
-                string[] minWords = new string[words.Length];
+                string minWords = "";                            // Пустая выводная строка
 
-                foreach (string word in words)          // Ищем все подходящие в массиве и добовляем в выводной массив
+                foreach (string word in words)                   // Ищем все подходящие в массиве и добовляем в выводной массив
                 {
                     if (word.Length == minLengthWord)
                     {
-                        minWords.Append(word);
+                        minWords += $"{word} ";                  // Добавляем к выводной строке минимальное слово
                     }
                 }
                 return minWords;
---
+++ Homework_05_02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_05_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(InputString(input));
+            Console.ReadKey();
+
+
+            string InputString(string inputString)
+            {
+                string outputString = "";
+                char prevLetter = ' ';
+
+                foreach (char letter in inputString)
+                {
+                    if (prevLetter != letter)
+                    {
+                        outputString += letter;
+                    }
+                    prevLetter = letter;
+                }
+                return outputString;
+            }
+        }
+    }
+}
