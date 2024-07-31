bd6e8182b6685e6cb5fd4e598cbd7ab0dc906ee2
dwarowski <realcarenthusiast@yandex.ru>
2024-07-31 04:51:30 +0300

    Created new proj

---
+++ Homework_05_03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_05_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+           void ArifmeticalProgrees()
+            {
+
+            }
+
+        }
+    }
+}
