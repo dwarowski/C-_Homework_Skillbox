16daa504553b77e2af267fee5269001d22917e15
dwarowski <realcarenthusiast@yandex.ru>
2024-09-18 11:01:24 +0300

    started hw6

---
+++ Homework_06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+
+                for (int j = 0; j < 50; j++)
+                {
+                    if(i % 10)
+                    {
+
+                    }
+                }
+            }
+
+        }
+    }
+}
